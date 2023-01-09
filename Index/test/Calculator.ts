57cfbb6ae83e49af1a936bbb77553eb8c31cbc21
mlalma <lassi.maksimainen@gmail.com>
2023-01-09 08:59:18 +0100

    Added rest of AbsoluteSpotIndexCalculator tests

--- Index/test/Calculator.ts
+++ Index/test/Calculator.ts
@@ -41,7 +41,7 @@ describe("Calculators", function () {
 
     describe("AbsoluteSpotIndexCalculator", function () {
 
-        it("Should correctly calculate the position of single underlying index", async function () {
+        it("Should correctly calculate the value of a single underlying index", async function () {
             let oracleVals = [0, 1, -1, 2, -2, 3, -3, 4, -4, 5, -5];
             oracleVals.forEach((value, _index, _array) => { value * Math.pow(10, 6) });
             await oracles[0].setVals(oracleVals);
@@ -67,6 +67,67 @@ describe("Calculators", function () {
                 const val = await absoluteSpotCalculator.connect(index.address).calculateIndex(oracleStorage1, indexStorage1, 1);
                 expect(val).to.eq(oracleVals[i] * Math.pow(10, 4));
                 await oracles[0].increasePos();
+            }
+        });
+
+        it("Should correctly calculate the value index consisting of multiple underlying sources", async function () {
+            let oracleVals = [0, 1, -1, 2, -2, 3, -3, 4, -4, 5, -5];
+            let oracleVals2 = [0, 2, -2, 3, -7, 5, -6, 5, -8, 9, -15];
+
+            oracleVals.forEach((value, _index, _array) => { value * Math.pow(10, 6) });
+            oracleVals2.forEach((value, _index, _array) => { value * Math.pow(10, 6) });
+            await oracles[0].setVals(oracleVals);
+            await oracles[1].setVals(oracleVals2);
+
+            await index.createOracleStorage([oracleAddresses[0], oracleAddresses[1]], absoluteSpotCalculator.address, noFix.address);
+            const oracleStg1 = await index.calculateOracleIndex([oracleAddresses[0], oracleAddresses[1]], absoluteSpotCalculator.address, noFix.address);
+            await index.createSpotIndex(oracleStg1, [WEIGHT_MULTIPLIER / 2, WEIGHT_MULTIPLIER / 2], []);
+            await index.fixIndex(1, [0, 0])
+
+            const oracleStorage1 = await index.getOracleStorage(oracleStg1);
+            const indexStorage1 = await index.getIndexStorage(1);
+
+            for (let i = 0; i < oracleVals.length; i++) {
+                await time.increase(1);
+                const val = await absoluteSpotCalculator.connect(index.address).calculateIndex(oracleStorage1, indexStorage1, 1);
+                expect(val).to.eq(oracleVals[i] * Math.pow(10, 4) / 2 + oracleVals2[i] * Math.pow(10, 4) / 2);
+                await oracles[0].increasePos();
+                await oracles[1].increasePos();
+            }
+        });
+
+        it("Should correctly calculate the value index consisting of multiple underlying sources w/ different weightings", async function () {
+            let oracleVals = [0, 1, -1, 2, -2, 3, -3, 4, -4, 5, -5];
+            let oracleVals2 = [0, 2, -2, 3, -7, 5, -6, 5, -8, 9, -15];
+            let oracleVals3 = [10, 42, -12, 33, -27, 35, -46, 55, -78, 89, -115];
+
+            oracleVals.forEach((value, _index, _array) => { value * Math.pow(10, 6) });
+            oracleVals2.forEach((value, _index, _array) => { value * Math.pow(10, 6) });
+            oracleVals3.forEach((value, _index, _array) => { value * Math.pow(10, 6) });
+            await oracles[0].setVals(oracleVals);
+            await oracles[1].setVals(oracleVals2);
+            await oracles[2].setVals(oracleVals3);
+
+            await index.createOracleStorage([oracleAddresses[0], oracleAddresses[1], oracleAddresses[2]], absoluteSpotCalculator.address, noFix.address);
+            const oracleStg1 = await index.calculateOracleIndex([oracleAddresses[0], oracleAddresses[1], oracleAddresses[2]], absoluteSpotCalculator.address, noFix.address);
+            await index.createSpotIndex(oracleStg1, [WEIGHT_MULTIPLIER / 2, WEIGHT_MULTIPLIER / 4, WEIGHT_MULTIPLIER / 4], []);
+            await index.fixIndex(1, [10 * Math.pow(10, 6), -2 * Math.pow(10, 6), 3 * Math.pow(10, 6)]);
+
+            const oracleStorage1 = await index.getOracleStorage(oracleStg1);
+            const indexStorage1 = await index.getIndexStorage(1);
+
+            for (let i = 0; i < oracleVals.length; i++) {
+                await time.increase(1);
+                const val = await absoluteSpotCalculator.connect(index.address).calculateIndex(oracleStorage1, indexStorage1, 1);
+
+                const val1 = (oracleVals[i] * Math.pow(10, 4) - 10 * Math.pow(10, 10)) / 2;
+                const val2 = (oracleVals2[i] * Math.pow(10, 4) + 2 * Math.pow(10, 10)) / 4;
+                const val3 = (oracleVals3[i] * Math.pow(10, 4) - 3 * Math.pow(10, 10)) / 4;
+
+                expect(val).to.eq(val1 + val2 + val3);
+                await oracles[0].increasePos();
+                await oracles[1].increasePos();
+                await oracles[2].increasePos();
             }
         });
     });
