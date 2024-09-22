3bd14416f8a812ab6163719d6bee7450f44f3227
mlalma <lassi.maksimainen@gmail.com>
2024-09-22 14:26:21 +0200

    Updates to contracts. Started Utils project where date-time functionality will be first added and which other tentofi projects will start using

---
+++ Utils/hardhat.config.ts
@@ -0,0 +1,30 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+
+require('dotenv').config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.27",
+  paths: {
+    sources: "./v1"
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1000
+    }
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true
+    },
+    polygon_mumbai: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
+}
